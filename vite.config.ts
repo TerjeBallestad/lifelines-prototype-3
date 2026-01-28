47afbdc7ef5a7a6bc60f130229c82bb13e908765
TerjeBallestad <terj.ba@gmail.com>
2026-01-28 18:15:34 +0100

    setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import observerPlugin from 'mobx-react-observer/babel-plugin'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import observerPlugin from 'mobx-react-observer/babel-plugin';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [
-          'babel-plugin-react-compiler',
-          observerPlugin(),
-        ],
+        plugins: ['babel-plugin-react-compiler', observerPlugin()],
       },
     }),
     tailwindcss(),
   ],
-})
+});
